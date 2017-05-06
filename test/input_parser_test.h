b4da1afe274ecac9173294f9b0533d1576bcba4b
Rudolf Biczok <rcbiczok@gmail.com>
2017-05-07 00:50:43 +0200

    Fix failing test

--- test/input_parser_test.h
+++ test/input_parser_test.h
@@ -1,8 +1,11 @@
 
 #include "gtest/gtest.h"
 #include "gmock/gmock-matchers.h"
-#include <limits.h>
+
 #include "../input_parser.h"
+
+#include <limits.h>
+#include <iostream>
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wglobal-constructors"
@@ -10,13 +13,15 @@
 // Test a simple tree file
 //TODO figure out, why i get a compiler error
 TEST(GetNewickTreeTest, simple_tree) {
-  ntree_t *tree = get_newk_tree("simple_tree.nwk");
+  ntree_t *tree = get_newk_tree("test/simple_tree.nwk");
 
-  EXPECT_EQ(tree->children[0]->label, "A");
-  EXPECT_EQ(tree->children[1]->label, "B");
-  EXPECT_EQ(tree->children[2]->children[0]->label, "C");
-  EXPECT_EQ(tree->children[2]->children[0]->label, "D");
-  EXPECT_EQ(tree->children[2]->children[1]->parent->parent->children[0]->label, "A");
+  std::cout << tree->children[0]->label << std::endl;
+
+  ASSERT_STREQ(tree->children[0]->label, "A");
+  ASSERT_STREQ(tree->children[1]->label, "B");
+  ASSERT_STREQ(tree->children[2]->children[0]->label, "C");
+  ASSERT_STREQ(tree->children[2]->children[1]->label, "D");
+  ASSERT_STREQ(tree->children[2]->children[1]->parent->parent->children[0]->label, "A");
 }
 
 #pragma clang diagnostic pop
