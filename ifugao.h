7049807da4a8d96d83b19230ec590213d7beb46c
axel <updky@student.kit.edu>
2017-05-06 17:31:00 +0200

    Added a simple binary tree structure.

--- ifugao.h
+++ ifugao.h
@@ -1,7 +1,10 @@
 typedef int leaf;
 
 public struct tree {
-  
+	bool is_leaf; // 0 if node is an internal node, 1 if node is a leaf
+	char* label; // name of the leaf, NULL pointer for an internal node
+	tree* left_subtree; // NULL pointer for leaf
+	tree* right_subtree; // NULL pointer for leaf
 } tree
 
 public struct constraint {
@@ -22,7 +25,7 @@ std::set<tree> combine_sets(const std::vector<std::shared_ptr<std::set>> &set_ar
 std::vector<std::shared_ptr<std::set>> apply_constraints(const std::vector<std::shared_ptr<std::set>> &set_array, const std::vector<constraint> &constraints);
 
 /** Returns a vector containing all constraints that still are valid for the given set of leaves */
-std::vector<constraint> find_constraints(const std::set &set, const std::vector<constraint> &constraints);
+std::vector<constraint> find_constraints(const std::set<leaf> &set, const std::vector<constraint> &constraints);
 
 /** merges two sub-trees */
 std::set<tree> merge_subtrees(std::set<tree> &left, std::set<tree> &right);
