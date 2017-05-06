f4f0a683f53b4de3eac0d4ddd50e06dae3b4f937
axel <updky@student.kit.edu>
2017-05-06 18:50:30 +0200

    Changed ifugao header file, now using vector<binary_tree> instead of set<binary_tree>

--- ifugao.h
+++ ifugao.h
@@ -1,18 +1,25 @@
+#ifndef IFUGAO_H
+#define IFUGAO_H
+
+#include "vector"
+#include "set"
+#include "tr1/memory"
+
 typedef int leaf;
 
-public struct tree {
+struct binary_tree {
 	bool is_leaf; // 0 if node is an internal node, 1 if node is a leaf
 	char* label; // name of the leaf, NULL pointer for an internal node
-	tree* left_subtree; // NULL pointer for leaf
-	tree* right_subtree; // NULL pointer for leaf
-} tree
+	binary_tree* left_subtree; // NULL pointer for leaf
+	binary_tree* right_subtree; // NULL pointer for leaf
+};
 
-public struct constraint {
+struct constraint {
   leaf smaller_left;
   leaf bigger_left;
   leaf smaller_right;
   leaf bigger_right;
-}
+};
 
 /** Calls apply_constraints first, then combine_sets and writes all found trees.
   @param count_only Only count trees, don't write them
@@ -20,16 +27,17 @@ public struct constraint {
 long list_trees(const std::vector<constraint> &constraints, std::vector<long> &leafs, bool count_only, FILE &file);
 
 /** Combines all sets (constraints need to be applied already) */
-std::set<tree> combine_sets(const std::vector<std::shared_ptr<std::set>> &set_array, const std::vector<constraint> &constraints)
+std::vector<binary_tree> combine_sets(const std::vector<std::tr1::shared_ptr<std::set<leaf> > > &set_array, const std::vector<constraint> &constraints);
 
-std::vector<std::shared_ptr<std::set>> apply_constraints(const std::vector<std::shared_ptr<std::set>> &set_array, const std::vector<constraint> &constraints);
+std::vector<std::tr1::shared_ptr<std::set<leaf> > > apply_constraints(const std::vector<std::tr1::shared_ptr<std::set<leaf> > > &set_array, const std::vector<constraint> &constraints);
 
 /** Returns a vector containing all constraints that still are valid for the given set of leaves */
 std::vector<constraint> find_constraints(const std::set<leaf> &set, const std::vector<constraint> &constraints);
 
 /** merges two sub-trees */
-std::set<tree> merge_subtrees(std::set<tree> &left, std::set<tree> &right);
+std::vector<binary_tree> merge_subtrees(std::vector<binary_tree> &left, std::vector<binary_tree> &right);
 
 /** write the given tree to the gien file */
-void write_tree(tree &tree, FILE &file);
+void write_tree(binary_tree &tree, FILE &file);
 
+#endif /* IFUGAO_H */
