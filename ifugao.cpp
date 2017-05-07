6b371ba5be78441fcd02519d49498aafd4dc4a9a
axel <updky@student.kit.edu>
2017-05-07 15:10:54 +0200

    implemented apply_constraints

--- ifugao.cpp
+++ ifugao.cpp
@@ -19,3 +19,40 @@ std::vector<binary_tree> merge_subtrees(std::vector<binary_tree> &left, std::vec
 	assert(merged_trees.size() == left.size() * right.size());
 	return merged_trees;
 }
+
+std::vector<std::tr1::shared_ptr<std::set<leaf> > > apply_constraints(const std::set<leaf> &leaves, const std::vector<constraint> &constraints) {
+	
+	std::vector<std::tr1::shared_ptr<std::set<leaf> > > sets;
+
+	for(leaf l: leaves) {
+		// create an empty set for each leave
+		std::tr1::shared_ptr<std::set<leaf>> set (new std::set<leaf>);
+		set->insert(l);
+		sets.push_back(set);
+	}
+
+
+	int index_containing_left_constraint;
+	int index_containing_right_constraint;
+
+	for(constraint cons: constraints) {
+		for(int i = 0; i < sets.size(); i++) {
+			if(sets[i]->find(cons.smaller_left) != sets[i]->end()) {
+				// set contains the left constraint
+				index_containing_left_constraint = i;
+			}
+			if(sets[i]->find(cons.smaller_right) != sets[i]->end()) {
+				// set contains the right constraint
+				index_containing_right_constraint = i;
+			}
+		}
+		assert(index_containing_left_constraint >= 0 && index_containing_left_constraint < sets.size());
+		assert(index_containing_right_constraint >= 0 && index_containing_right_constraint < sets.size());
+		if(index_containing_left_constraint != index_containing_right_constraint) {
+			// sets need to be merged
+			sets[index_containing_left_constraint]->insert(sets[index_containing_right_constraint]->begin(), sets[index_containing_right_constraint]->end());
+			sets.erase(sets.begin() + index_containing_right_constraint);
+		}
+	}
+	return sets;
+}
