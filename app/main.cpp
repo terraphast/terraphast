b906e30a1222da1b82deb212a67a950f74c0b2cb
Florian Weber <oss@florianjw.de>
2017-05-14 04:11:21 +0200

    have the app parse actual data

--- app/main.cpp
+++ app/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+#include <fstream>
 
 #include <terraces/nodes.hpp>
+#include <terraces/parser.hpp>
 
-int main() {
-	const auto tree = terraces::tree{
-		{},
-		{0},
-		{0},
-		{2},
-	};
-
-	for (const auto& node : tree) {
-		std::cout << node << '\n';
+int main(int argc, char** argv) {
+	for (int i = 1; i < argc; ++i) {
+		std::cout << "reading file " << argv[i] << "\n";
+		auto line = std::string{};
+		auto file = std::ifstream{argv[i]};
+		while(std::getline(file, line)) {
+			auto data = terraces::parse_nwk(line);
+			for (auto i = terraces::index{}; i < data.first.size(); ++i) {
+				std::cout
+					<< i << " (" << data.second.at(i)
+					<< ") : " << data.first.at(i).data.at(0) << "\n";
+			}
+		}
 	}
 
 }
