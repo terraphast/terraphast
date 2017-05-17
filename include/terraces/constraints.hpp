f0a2bd17015bab593de6f0f0ab67e99a86c13829
Fedor Scholz <fsc@giraffenmail.de>
2017-05-17 18:06:42 +0200

    add contraint types

---
+++ include/terraces/constraints.hpp
@@ -0,0 +1,20 @@
+#ifndef TERRACES_CONSTRAINTS_HPP
+#define TERRACES_CONSTRAINTS_HPP
+
+#include <vector>
+
+#include "trees.hpp"
+
+namespace terraces {
+
+struct constraint {
+        index shared;
+        index left;
+        index right;
+};
+
+using constraints = std::vector<constraint>;
+
+} // namespace terraces
+
+#endif
