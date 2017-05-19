df310b7636fb6e9241c5b13ee9ceffb396febee5
Florian Weber <oss@florianjw.de>
2017-05-19 23:41:04 +0200

    add basic interface for rerooting-algorithm

---
+++ include/terraces/rooting.hpp
@@ -0,0 +1,12 @@
+#ifndef TERRACES_ROOTING_HPP
+#define TERRACES_ROOTING_HPP
+
+#include "trees.hpp"
+
+namespace terraces {
+
+void reroot_inplace(tree& t, index root_leaf);
+
+} // namespace terraces
+
+#endif
---
+++ lib/rooting.cpp
@@ -0,0 +1,13 @@
+
+#include <terraces/rooting.hpp>
+
+namespace terraces {
+
+void reroot_inplace(tree& t, index root_leaf) {
+	(void) t;
+	(void) root_leaf;
+}
+
+} // namespace terraces
+
+
