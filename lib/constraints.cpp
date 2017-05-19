e0e9306b300839a685123d9e03c0817276161ecc
Fedor Scholz <fsc@giraffenmail.de>
2017-05-19 15:07:31 +0200

    reformat

--- lib/constraints.cpp
+++ lib/constraints.cpp
@@ -4,17 +4,17 @@
 namespace terraces {
 
 constraints filter_constraints(std::vector<index> leaves, constraints c) {
-        constraints new_c;
-        for (size_t i = 0; i < c.size(); i++) {
-                if ((std::find(leaves.begin(), leaves.end(), c.at(i).shared) != leaves.end()) && (std::find(leaves.begin(), leaves.end(), c.at(i).left) != leaves.end())) {
-                        new_c.push_back(c.at(i));
-                }
-        }
-        return new_c;
+	constraints new_c;
+	for (size_t i = 0; i < c.size(); i++) {
+		if ((std::find(leaves.begin(), leaves.end(), c.at(i).shared) != leaves.end()) &&
+		    (std::find(leaves.begin(), leaves.end(), c.at(i).left) != leaves.end())) {
+			new_c.push_back(c.at(i));
+		}
+	}
+	return new_c;
 }
 
-std::vector<std::vector<index>> apply_constraints(std::vector<index> species,
-                                                  constraints c) {
+std::vector<std::vector<index>> apply_constraints(std::vector<index> species, constraints c) {
 	union_find leaves = make_set(species.size());
 	for (size_t i = 0; i < c.size(); i++) {
 		merge(leaves, c.at(i).shared, c.at(i).left);
--- test/constraints.cpp
+++ test/constraints.cpp
@@ -10,10 +10,10 @@ namespace tests {
 TEST_CASE("constraints", "[constraints]") {
 	std::vector<index> leaves1 = {0, 1, 2};
 	std::vector<index> leaves2 = {0, 1, 2, 3};
-	constraints c = { constraint{7, 8, 9}, constraint{0, 1, 2}, constraint{1, 3, 2} };
+	constraints c = {constraint{7, 8, 9}, constraint{0, 1, 2}, constraint{1, 3, 2}};
 
-	std::vector<std::vector<index>> target_leaves1 = { {0, 1}, {2} };
-	std::vector<std::vector<index>> target_leaves2 = { {0, 1, 3}, {2} };
+	std::vector<std::vector<index>> target_leaves1 = {{0, 1}, {2}};
+	std::vector<std::vector<index>> target_leaves2 = {{0, 1, 3}, {2}};
 
 	CHECK(apply_constraints(leaves1, filter_constraints(leaves1, c)) == target_leaves1);
 	CHECK(apply_constraints(leaves2, filter_constraints(leaves2, c)) == target_leaves2);
