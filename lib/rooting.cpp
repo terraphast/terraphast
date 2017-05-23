16c285c558388a647683654b97aa513dc3d3a483
Peter Bozsoky <inbox+github@0rly.eu>
2017-05-23 15:38:35 +0200

    Fixed rooting for the simple test cases.

--- lib/rooting.cpp
+++ lib/rooting.cpp
@@ -1,4 +1,3 @@
-
 #include <stack>
 #include <iostream>
 #include <terraces/rooting.hpp>
@@ -19,60 +18,93 @@ void reroot_inplace(tree& t, index root_leaf) {
 	utils::ensure<std::invalid_argument>(t[root_leaf].rchild() == none, 
 										 "The given index root_leaf is no leaf index.");
 	
-	node old_root = t[0];
-	node new_root = node{none, t[root_leaf].parent(), root_leaf};
-	t[root_leaf].parent() = 0;
-
+	node old_root = t[0]; 			// Copy
 	
-	bool coming_from_left = true;
-	if (root_leaf == t[t[root_leaf].parent()].rchild()) {
-		coming_from_left = false;
+	bool root_leaf_is_child_of_root = (old_root.lchild() == root_leaf || old_root.rchild() == root_leaf);
+	if (root_leaf_is_child_of_root) {
+		return;
 	}
 	
+	(void) old_root;
+	node new_root = node{none, none, none};
+	node& leaf_node = t[root_leaf];	// Alias/Reference
+	
+	bool coming_from_left = true;
+	if (root_leaf == t[leaf_node.parent()].rchild()) {
+		coming_from_left = false;
+	}
+
+	
 	bool is_first_node = true;
+	bool is_last_node = false;
 	index current_node_index = t[root_leaf].parent();
-	node current_node = t[current_node_index];
-	index original_parent_index = current_node.parent();
-	std::cout << "Going upwards...";
+	std::cout << "Going upwards...\n";
 	while (current_node_index != 0) {
-		std::cout << "current_node_index BEGIN: " << current_node_index << "\n";
-		std::cout << "current_parent_index BEGIN: " << original_parent_index << "\n";
-		std::cout << "current_node's parent BEGIN: " << current_node.parent() << "\n";
-		std::cout << "current_node's lchild BEGIN: " << current_node.lchild() << "\n";
-		std::cout << "current_node's rchild BEGIN: " << current_node.rchild() << "\n";
+
+		node& current_node = t[current_node_index];
+		index original_parent_index = current_node.parent();
+		is_last_node = (original_parent_index == 0);
+		std::cout << "IS LAST " << is_last_node << "\n";
+		std::cout << "IS FIRST " << is_first_node << "\n";
+		std::cout << "FROM LEFT? " << coming_from_left << "\n";
+
+		// standard case: Swap child and parent indices.
 		if (coming_from_left) {
 			current_node.parent() = current_node.lchild();
 			current_node.lchild() = original_parent_index;
-			t[current_node.lchild()].parent() = current_node_index;
 		} else {
 			current_node.parent() = current_node.rchild();
 			current_node.rchild() = original_parent_index;
-			t[current_node.rchild()].parent() = current_node_index;
 		}
-		if (is_first_node) {
-			current_node.parent() = 0;
+
+		// Direct parent of root_leaf
+		// Overwrites our parent
+		if (is_first_node) { 
+			leaf_node.parent() = 0;
+			new_root.lchild() = current_node_index;
+		 	new_root.rchild() = root_leaf;
+			current_node.parent() = 0; // CHECK
+			if (coming_from_left) {
+				current_node.lchild() = original_parent_index;
+			} else {
+				current_node.rchild() = original_parent_index;
+			}
 		}
-		if (t[original_parent_index].rchild() == current_node_index) {
-			coming_from_left = false;
-		} else {
+		
+		// Directly under old_root: We're now the parent of the root's other child.
+		// Overwrites the approrpiate child index.
+		if (is_last_node) { 
+			if (coming_from_left) { // we swap out our left child...
+				if (old_root.lchild() == current_node_index) { //...for the old root's right child.
+					current_node.lchild() = old_root.rchild();
+				} else { //...for the old root's left child
+					current_node.lchild() = old_root.lchild();
+				}
+				t[current_node.lchild()].parent() = current_node_index;
+
+			} else { // we swap out out right child...
+				if (old_root.lchild() == current_node_index) { //...for the old root's right child.
+					current_node.rchild() = old_root.rchild();
+				} else { //...for the old root's left child
+					current_node.rchild() = old_root.lchild();
+				}
+				t[current_node.rchild()].parent() = current_node_index;
+			}
+		}
+
+		// CLEANUP
+		if (t[original_parent_index].lchild() == current_node_index) {
 			coming_from_left = true;
+		} else {
+			coming_from_left = false;
 		}
-		std::cout << "current_node's parent END: " << current_node.parent() << "\n";
-		std::cout << "current_node's lchild END: " << current_node.lchild() << "\n";
-		std::cout << "current_node's rchild END: " << current_node.rchild() << "\n";
 		current_node_index = original_parent_index;
-		current_node = t[current_node_index];
-		original_parent_index = current_node.parent();
-		std::cout << "current_node_index END: " << current_node_index << "\n";
-		std::cout << "current_parent_index END: " << original_parent_index << "\n";
 	}
 	std::cout << "finished\n";
+	std::cout << "FROM LEFT END? " << coming_from_left << "\n";
 
-	t[old_root.lchild()].parent() = old_root.rchild();
-	t[old_root.rchild()].lchild() = old_root.lchild();
 	t[0] = new_root;
+	std::cout << "ROOT END:" << t[0] << "\n";
 }
 
 } // namespace terraces
-
-
--- test/rooting.cpp
+++ test/rooting.cpp
@@ -10,14 +10,30 @@ namespace tests {
 
 TEST_CASE("rerooting basic", "[rerooting]") {
 	auto t = tree{{none, 1, 2}, {0, none, none}, {0, 3, 4}, {2, none, none}, {2, none, none}};
+	auto t_2 = tree(t);
+	auto t_3 = tree(t);
+	
 	const auto root_leaf = 3;
+	std::cout << "==========root_leaf==" << root_leaf << "\n";
 	reroot_inplace(t, root_leaf);
-
-	// TODO: implement the algorithm, make this work and add further tests:
 	CHECK(((t[0].lchild() == root_leaf) or (t[0].rchild() == root_leaf)));
-	std::cout << t[0] << "\n" << t[1] << "\n" << t[2] << "\n" << t[3] << "\n" << t[4];
-	std::cout << t[0].parent() + 1;
+	std::cout << t[0] << "\n" << t[1] << "\n" << t[2] << "\n" << t[3] << "\n" << t[4] << "\n";
+
+	const auto root_leaf_2 = 4;
+	std::cout << "==========root_leaf==" << root_leaf_2 << "\n";
+	reroot_inplace(t_2, root_leaf_2);
+	CHECK(((t_2[0].lchild() == root_leaf_2) or (t_2[0].rchild() == root_leaf_2)));
+	std::cout << t_2[0] << "\n" << t_2[1] << "\n" << t_2[2] << "\n" << t_2[3] << "\n" << t_2[4] << "\n";
+
+	const auto root_leaf_3 = 1;
+	std::cout << "==========root_leaf==" << root_leaf_3 << "\n";
+	reroot_inplace(t_3, root_leaf_3);
+	CHECK(((t_3[0].lchild() == root_leaf_3) or (t_3[0].rchild() == root_leaf_3)));
+	std::cout << t_3[0] << "\n" << t_3[1] << "\n" << t_3[2] << "\n" << t_3[3] << "\n" << t_3[4] << "\n";
+
 }
+
+
 
 } // namespace tests
 } // namespace terraces
