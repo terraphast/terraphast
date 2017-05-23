c1926b7503aeb24ef97844715c443593bc989797
Peter Bozsoky <inbox+github@0rly.eu>
2017-05-23 18:26:51 +0200

    Added function for testing child-parent-edges.

--- test/rooting.cpp
+++ test/rooting.cpp
@@ -8,6 +8,15 @@
 namespace terraces {
 namespace tests {
 
+bool parent_child_relationship(tree& t, index parent, index child) {
+	bool left_child = t[parent].lchild() == child;
+	if (left_child) {
+		return t[t[parent].lchild()].parent() == parent;
+	}
+	bool right_child = t[parent].rchild() == child;
+	return right_child && t[t[parent].rchild()].parent() == parent;
+}
+
 TEST_CASE("rerooting basic", "[rerooting]") {
 	auto t = tree{{none, 1, 2}, {0, none, none}, {0, 3, 4}, {2, none, none}, {2, none, none}};
 	auto t_2 = tree(t);
@@ -16,6 +25,10 @@ TEST_CASE("rerooting basic", "[rerooting]") {
 	const auto root_leaf = 3;
 	reroot_inplace(t, root_leaf);
 	CHECK(((t[0].lchild() == root_leaf) or (t[0].rchild() == root_leaf)));
+	CHECK(parent_child_relationship(t, 0, root_leaf));
+	CHECK(parent_child_relationship(t, 0, 2));
+	CHECK(parent_child_relationship(t, 2, 4));
+	CHECK(parent_child_relationship(t, 2, 1));
 
 	const auto root_leaf_2 = 4;
 	reroot_inplace(t_2, root_leaf_2);
