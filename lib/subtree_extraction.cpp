483fac8024ff5e87add2ce0fa3d7b9043d179143
upsj <mail@upsj.de>
2017-05-23 17:21:44 +0200

    add buggy subtree extraction

--- lib/subtree_extraction.cpp
+++ lib/subtree_extraction.cpp
@@ -1,31 +1,59 @@
 #include <terraces/subtree_extraction.hpp>
 
+using std::vector;
+using std::stack;
+using std::pair;
+using std::make_pair;
+
 namespace terraces {
 
 std::vector<tree> subtrees(const tree& t, const bitmatrix& occ) {
-	auto num_species = occ.rows();
+	auto num_nodes = occ.rows();
 	auto num_sites = occ.cols();
-	assert(t.size() == 2 * num_species - 2);
+	assert(t.size() == num_nodes);
 	assert(is_rooted_tree(t));
 
-	std::vector<tree> out_trees{num_sites, tree{}};
+	vector<tree> out_trees(num_sites, tree(num_nodes, node{}));
+	vector<stack<index>> tree_boundaries{num_sites, stack<index>{}};
 
 	auto node_occ = occ;
 
+	// compute occurrences on inner nodes: bitwise or of the children
 	foreach_postorder(t, [&](index i) {
-		auto lchild = t[i].lchild();
-		auto rchild = t[i].rchild();
-		node_occ.row_or(lchild, rchild, i);
+		auto node = t[i];
+		if (!is_leaf(node)) {
+			node_occ.row_or(node.lchild(), node.rchild(), i);
+		}
+	});
+
+	// collect leaves and inner nodes: bitwise and of the children
+	foreach_preorder(t, [&](index i) {
+		auto node = t[i];
 		for (index site = 0; site < num_sites; ++site) {
-			if (node_occ.get(lchild, site) && node_occ.get(rchild, site)) {
-				// TODO insert node in the tree for site
+			auto& out_tree = out_trees[site];
+			auto& boundary = tree_boundaries[site];
+
+			if (node_occ.get(i, site) && !is_root(node)) {
+				/* Nodes which have this site:
+				 * Link them to the lowest node on the right boundary.
+				 * It should have at most one child already linked. */
+				auto parent = boundary.top();
+				out_tree[i].parent() = parent;
+				if (out_tree[parent].lchild() == none) {
+					out_tree[parent].lchild() = i;
+				} else {
+					assert(out_tree[parent].rchild() == none);
+					out_tree[parent].rchild() = i;
+					boundary.pop();
+				}
 			}
-			if (!node_occ.get(i, site)) {
-				// TODO ignore this branch from here on for site
+
+			bool insert_node = !is_leaf(node) && node_occ.get(node.lchild(), site) &&
+			                   node_occ.get(node.rchild(), site);
+			if (insert_node) {
+				boundary.push(i);
 			}
 		}
-
-		// TODO work out clever indexing scheme for subtrees ;)
 	});
 
 	return out_trees;
