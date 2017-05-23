5154f7b19c351e2779dc11bf41fc06934e7d83e9
upsj <mail@upsj.de>
2017-05-23 20:11:44 +0200

    add debug output for subtree extraction

--- lib/subtree_extraction.cpp
+++ lib/subtree_extraction.cpp
@@ -1,5 +1,7 @@
 #include <terraces/subtree_extraction.hpp>
 
+#include <iostream>
+
 using std::vector;
 using std::stack;
 using std::pair;
@@ -14,7 +16,7 @@ std::vector<tree> subtrees(const tree& t, const bitmatrix& occ) {
 	assert(is_rooted_tree(t));
 
 	vector<tree> out_trees(num_sites, tree(num_nodes, node{}));
-	vector<stack<index>> tree_boundaries{num_sites, stack<index>{}};
+	vector<vector<index>> tree_boundaries{num_sites, vector<index>{}};
 
 	auto node_occ = occ;
 
@@ -28,6 +30,13 @@ std::vector<tree> subtrees(const tree& t, const bitmatrix& occ) {
 
 	// collect leaves and inner nodes: bitwise and of the children
 	foreach_preorder(t, [&](index i) {
+		std::cout << i << ": ";
+		for (auto n : tree_boundaries[0])
+			std::cout << n << " ";
+		std::cout << "\n" << i << ": ";
+		for (auto n : tree_boundaries[1 % num_sites])
+			std::cout << n << " ";
+		std::cout << "\n";
 		auto node = t[i];
 		for (index site = 0; site < num_sites; ++site) {
 			auto& out_tree = out_trees[site];
@@ -37,21 +46,21 @@ std::vector<tree> subtrees(const tree& t, const bitmatrix& occ) {
 				/* Nodes which have this site:
 				 * Link them to the lowest node on the right boundary.
 				 * It should have at most one child already linked. */
-				auto parent = boundary.top();
+				auto parent = boundary.back();
 				out_tree[i].parent() = parent;
 				if (out_tree[parent].lchild() == none) {
 					out_tree[parent].lchild() = i;
 				} else {
 					assert(out_tree[parent].rchild() == none);
 					out_tree[parent].rchild() = i;
-					boundary.pop();
+					boundary.pop_back();
 				}
 			}
 
 			bool insert_node = !is_leaf(node) && node_occ.get(node.lchild(), site) &&
 			                   node_occ.get(node.rchild(), site);
 			if (insert_node) {
-				boundary.push(i);
+				boundary.push_back(i);
 			}
 		}
 	});
