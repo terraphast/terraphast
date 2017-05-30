96ad006e316becf2876d12fefb5f50c3e4c2fc7f
upsj <mail@upsj.de>
2017-05-30 08:51:10 +0200

    add integration test based on alexis' example
    TODO: should it return 15 or 17?

---
+++ test/integration.cpp
@@ -0,0 +1,29 @@
+#include <catch.hpp>
+
+#include <terraces/constraints.hpp>
+#include <terraces/rooting.hpp>
+#include <terraces/subtree_extraction.hpp>
+#include <terraces/supertree.hpp>
+
+namespace terraces {
+
+namespace tests {
+
+TEST_CASE("full_run_disjoint", "[rerooting],[tree_extraction],[constraints],[supertree]") {
+	tree t{{none, 1, 2},    {0, 3, 4},       {0, none, none}, {1, 5, 6},      {1, 7, 8},
+	       {3, none, none}, {3, none, none}, {4, none, none}, {4, none, none}};
+	bitmatrix b{9, 2};
+	b.set(2, 0, true);
+	b.set(2, 1, true);
+	b.set(5, 1, true);
+	b.set(6, 1, true);
+	b.set(7, 0, true);
+	b.set(8, 0, true);
+	auto ts = subtrees(t, b);
+	auto cs = compute_constraints(ts);
+	CHECK(count_supertree(std::vector<index>{2, 5, 6, 7, 8}, cs) == 17);
+}
+
+} // namespace tests
+
+} // namespace terraces
