3876db2fbb94cb3a881674302667fab5b2b6b01c
Unknown <standart-02@web.de>
2017-06-12 18:21:12 +0200

    "Fixed" compiler error

--- src/leaf_set.cpp
+++ src/leaf_set.cpp
@@ -1,15 +1,15 @@
 #include "leaf_set.h"
 
 
-UnionFindLeafSet::apply_constraints(const std::vector<constraint> &constraints) {
+//std::vector<LeafSet> UnionFindLeafSet::apply_constraints(const std::vector<constraint> &constraints) {
 
-}
+//}
 
-UnionFindLeafSet::contains(size_t leaf) {
+bool UnionFindLeafSet::contains(size_t leaf) {
     return data_structure->find(leaf) == set;
 }
 
-UnionFindLeafSet::get_nth_partition_tuple(std::vector<LeafSet> &partitions, size_t n) {
+//std::tuple<LeafSet, LeafSet> UnionFindLeafSet::get_nth_partition_tuple(std::vector<LeafSet> &partitions, size_t n) {
 
-}
+//}
 
