909c03a85e81ce78528d3536e3026384679a9568
Unknown <standart-02@web.de>
2017-06-24 09:15:54 +0200

    Added tests for union find

---
+++ test/union_find_leaf_set_test.h
@@ -0,0 +1,77 @@
+#ifndef UNION_FIND_LEAF_SET_TEST_H
+#define UNION_FIND_LEAF_SET_TEST_H
+
+#include "leaf_set.h"
+#include "UnionFind.h"
+#include "ifugao.h"
+
+#include <vector>
+#include <iostream>
+
+TEST(get_nth_partition_tuple, no_constraints) {
+    UnionFind union_find(10);
+    UnionFindLeafSet leafset(union_find);
+    std::vector<constraint> constraints;
+    leafset.apply_constraints(constraints);
+
+    std::shared_ptr<LeafSet> part_left;
+    std::shared_ptr<LeafSet> part_right;
+    std::tie(part_left, part_right) = leafset.get_nth_partition_tuple(1);
+
+    ASSERT_EQ(part_left->contains(0), true);
+    ASSERT_EQ(part_right->contains(0), false);
+
+    for (int i = 1; i < 10; i++) {
+        ASSERT_EQ(part_left->contains(i), false);
+    }
+    for (int i = 1; i < 10; i++) {
+        ASSERT_EQ(part_right->contains(i), true);
+    }
+}
+
+TEST(get_nth_partition_tuple, only_one_set) {
+    UnionFind union_find(10);
+    UnionFindLeafSet leafset(union_find);
+    std::vector<constraint> constraints;
+    constraints.emplace_back(1,2,3,4);
+    constraints.emplace_back(2,3,3,4);
+    constraints.emplace_back(3,4,3,4);
+    constraints.emplace_back(4,5,3,4);
+    constraints.emplace_back(5,6,3,4);
+    constraints.emplace_back(6,7,3,4);
+    constraints.emplace_back(7,8,3,4);
+    constraints.emplace_back(8,9,3,4);
+
+    leafset.apply_constraints(constraints);
+
+    std::shared_ptr<LeafSet> part_left;
+    std::shared_ptr<LeafSet> part_right;
+    std::tie(part_left, part_right) = leafset.get_nth_partition_tuple(1);
+
+    for (int i = 0; i < 10; i++) {
+        ASSERT_EQ(part_left->contains(i), true);
+    }
+    for (int i = 0; i < 10; i++) {
+        ASSERT_EQ(part_right->contains(i), false);
+    }
+}
+
+TEST(get_nth_partition_tuple, only_one_leaf) {
+    UnionFind union_find(1);
+    UnionFindLeafSet leafset(union_find);
+    std::vector<constraint> constraints;
+
+    leafset.apply_constraints(constraints);
+
+    std::shared_ptr<LeafSet> part_left;
+    std::shared_ptr<LeafSet> part_right;
+    std::tie(part_left, part_right) = leafset.get_nth_partition_tuple(1);
+
+    ASSERT_EQ(part_left->contains(1), true);
+    ASSERT_EQ(part_right->contains(1), false);
+}
+
+//TODO more testcases, especially with a loop over all the partition tuples
+
+
+#endif // UNION_FIND_LEAF_SET_TEST_H
