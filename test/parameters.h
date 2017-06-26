f91caae05d2c44dcd8eaf7e3b18574430a67f111
Rudolf Biczok <rcbiczok@gmail.com>
2017-06-26 22:16:18 +0200

    Make template out of parameter class

---
+++ test/parameters.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef IFUGAO_PARAMETERS_H
+#define IFUGAO_PARAMETERS_H
+
+template <typename T>
+struct TerracesAnalysisTestParameter {
+    const char *newick_file;
+    const char *data_file;
+    const T expected_value;
+    TerracesAnalysisTestParameter(const char *p_newick_file,
+                                  const char *p_data_file,
+                                  T p_expected_value)
+            : newick_file(p_newick_file),
+              data_file(p_data_file),
+              expected_value(p_expected_value) {
+    }
+};
+
+#endif // IFUGAO_PARAMETERS_H
