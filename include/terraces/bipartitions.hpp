1d08439821a619aaa1b26ed9f30004d5ee794e36
Florian Weber <oss@florianjw.de>
2017-06-28 16:47:44 +0200

    add an allocator-parameter to bipartition_iterator

--- include/terraces/bipartitions.hpp
+++ include/terraces/bipartitions.hpp
@@ -1,8 +1,9 @@
 #ifndef TERRACES_BIPARTITIONS_HPP
 #define TERRACES_BIPARTITIONS_HPP
 
+#include <cassert>
 #include <cmath>
-#include <iosfwd>
+#include <ostream>
 
 #include "bitvector.hpp"
 #include "trees.hpp"
@@ -10,7 +11,11 @@
 
 namespace terraces {
 
-class bipartition_iterator {
+template <typename Alloc>
+class basic_bipartition_iterator {
+public:
+	using bitvector = basic_bitvector<Alloc>;
+
 private:
 	const bitvector& m_leaves;
 	const union_find& m_sets;
@@ -20,11 +25,13 @@ private:
 	index m_bip;
 	index m_end;
 
+	Alloc m_alloc;
+
 	bool in_left_partition(index i) const;
 	bitvector find_set_reps() const;
 
 public:
-	bipartition_iterator(const bitvector& leaves, const union_find& sets);
+	basic_bipartition_iterator(const bitvector& leaves, const union_find& sets, Alloc a = {});
 	/** Moves to the next bipartition. */
 	void increase();
 	/** Returns true if this bipartition is valid. */
@@ -41,7 +48,81 @@ public:
 	const bitvector& leaves() const { return m_leaves; }
 };
 
-std::ostream& operator<<(std::ostream& stream, const bipartition_iterator& it);
+template <typename Alloc>
+std::ostream& operator<<(std::ostream& stream, const basic_bipartition_iterator<Alloc>& it);
+
+using bipartition_iterator = basic_bipartition_iterator<std::allocator<index>>;
+
+template <typename Alloc>
+basic_bipartition_iterator<Alloc>::basic_bipartition_iterator(const bitvector& leaves,
+                                                              const union_find& sets, Alloc a)
+        : m_leaves{leaves}, m_sets{sets}, m_set_rep{find_set_reps()}, m_subleaves{leaves.size(), a},
+          m_bip{0}, m_end{(1ull << (m_set_rep.count() - 1))}, m_alloc{a} {
+	assert(m_set_rep.count() < 64);
+	increase();
+}
+
+template <typename Alloc>
+bool basic_bipartition_iterator<Alloc>::in_left_partition(index i) const {
+	return (m_bip & (1ull << (i - 1))) != 0;
+}
+
+template <typename Alloc>
+basic_bitvector<Alloc> basic_bipartition_iterator<Alloc>::find_set_reps() const {
+	// TODO ugly style :)
+	bitvector set_rep(m_leaves.count(), m_alloc);
+	for (index i = 0; i < m_leaves.count(); ++i) {
+		set_rep.set(m_sets.find(i));
+	}
+	set_rep.update_ranks();
+	return set_rep;
+}
+
+template <typename Alloc>
+bool basic_bipartition_iterator<Alloc>::is_valid() const {
+	return m_bip < m_end;
+}
+
+template <typename Alloc>
+void basic_bipartition_iterator<Alloc>::increase() {
+	m_bip++;
+	// TODO can we do better?
+	if (is_valid()) {
+		m_subleaves.blank();
+		index ii = 0;
+		for (auto i = m_leaves.first_set(); i < m_leaves.last_set();
+		     i = m_leaves.next_set(i)) {
+			if (in_left_partition(m_set_rep.rank(m_sets.find(ii)))) {
+				m_subleaves.set(i);
+			}
+			++ii;
+		}
+		m_subleaves.update_ranks();
+	}
+}
+
+template <typename Alloc>
+const basic_bitvector<Alloc>& basic_bipartition_iterator<Alloc>::get_current_set() const {
+	return m_subleaves;
+}
+
+template <typename Alloc>
+void basic_bipartition_iterator<Alloc>::flip_sets() {
+	m_subleaves.bitwise_xor(m_leaves);
+	m_subleaves.update_ranks();
+}
+
+template <typename Alloc>
+std::ostream& operator<<(std::ostream& stream, const basic_bipartition_iterator<Alloc>& it) {
+	for (index b = it.end_bip(), c = it.cur_bip(); b != 0u; b >>= 1, c >>= 1) {
+		stream << (index)(c & 1);
+	}
+	stream << '/';
+	for (index b = it.end_bip(); b != 0u; b >>= 1) {
+		stream << (index)(b & 1);
+	}
+	return stream;
+}
 
 } // namespace terraces
 
