a30b0cdfae9f9097cdc01f1687ad5e7c5263f78b
Tobias Ribizel <tobias.ribizel@student.kit.edu>
2017-07-13 19:59:34 +0200

    fix app test constructor usage

--- test/app.cpp
+++ test/app.cpp
@@ -22,49 +22,50 @@ using terraces::variants::multitree_callback;
 using terraces::variants::count_callback;
 using terraces::utils::as_comma_separated_output;
 using terraces::full_set;
+using std::make_tuple;
 
 namespace terraces {
 namespace tests {
 
 mpz_class helper(std::string, std::string);
 
-std::vector<std::tuple<std::string, std::string, mpz_class>> data_sets = {
-        {"Allium.nwk", "Allium.data", 8038035},
-        {"Allium_Reduced.nwk", "Allium_Reduced.data", 730680125},
-        {"Allium_Tiny.nwk", "Allium_Tiny.data", 35},
-        {"Asplenium.nwk.1", "Asplenium.data.1", 1},
-        {"Asplenium.nwk.2", "Asplenium.data.2", 95},
-        {"Bouchenak.nwk", "Bouchenak.data", 61261515},
-        {"Caryophyllaceae.nwk", "Caryophyllaceae.data", 718346120625},
-        {"Eucalyptus.nwk.1", "Eucalyptus.data.1", 229},
-        {"Eucalyptus.nwk.2", "Eucalyptus.data.2", 267},
-        {"Eucalyptus.nwk.3", "Eucalyptus.data.3", 9},
-        {"Euphorbia.nwk.1", "Euphorbia.data.1", 759},
-        {"Euphorbia.nwk.2", "Euphorbia.data.2", 759},
-        {"Ficus.nwk.1", "Ficus.data.1", 283815},
-        {"Ficus.nwk.2", "Ficus.data.2", 851445},
-        {"Ficus.nwk.3", "Ficus.data.3", 851445},
-        {"Iris.nwk", "Iris.data", 1},
-        {"Meredith.mammals.nwk", "Meredith.mammals.data", 1},
-        {"Meusemann.nwk", "Meusemann.data", 1},
-        {"Miadlikowska.fungi.nwk", "Miadlikowska.fungi.data", 11655},
-        {"Misof.insects.nwk", "Misof.insects.data", 1},
-        {"Primula.nwk", "Primula.data", 2835},
-        {"Pyron.nwk", "Pyron.data", 2205},
-        {"Rabosky.scincids.nwk", "Rabosky.scincids.data", 3},
-        {"Ranunculus.nwk", "Ranunculus.data", 3},
-        {"Rhododendron.nwk", "Rhododendron.data", 81},
-        {"Rosaceae.nwk", "Rosaceae.data", mpz_class{"171924246129452853684375"}},
-        {"Shi.bats.nwk", "Shi.bats.data", mpz_class{"242234933881884349476362274404296875"}},
-        {"Solanum.nwk", "Solanum.data", 211865625},
-        {"Springer.primates.nwk", "Springer.primates.data", 70840575},
-        {"Szygium.nwk.1", "Szygium.data.1", 45},
-        {"Szygium.nwk.2", "Szygium.data.2", 45},
-        {"Tolley.chameleons.nwk", "Tolley.chameleons.data", 1},
-        {"Wick.1kp.few.genes.nwk", "Wick.1kp.few.genes.data", 1},
-        {"Wick.1kp.many.genes.nwk", "Wick.1kp.many.genes.data", 1},
-        {"Yang.caryo.1122.nwk", "Yang.caryo.1122.data", 1},
-        {"Yang.caryo.209.nwk", "Yang.caryo.209.data", 1}};
+std::vector<std::tuple<std::string, std::string, mpz_class>> data_sets{
+        make_tuple("Allium.nwk", "Allium.data", 8038035),
+        make_tuple("Allium_Reduced.nwk", "Allium_Reduced.data", 730680125),
+        make_tuple("Allium_Tiny.nwk", "Allium_Tiny.data", 35),
+        make_tuple("Asplenium.nwk.1", "Asplenium.data.1", 1),
+        make_tuple("Asplenium.nwk.2", "Asplenium.data.2", 95),
+        make_tuple("Bouchenak.nwk", "Bouchenak.data", 61261515),
+        make_tuple("Caryophyllaceae.nwk", "Caryophyllaceae.data", 718346120625),
+        make_tuple("Eucalyptus.nwk.1", "Eucalyptus.data.1", 229),
+        make_tuple("Eucalyptus.nwk.2", "Eucalyptus.data.2", 267),
+        make_tuple("Eucalyptus.nwk.3", "Eucalyptus.data.3", 9),
+        make_tuple("Euphorbia.nwk.1", "Euphorbia.data.1", 759),
+        make_tuple("Euphorbia.nwk.2", "Euphorbia.data.2", 759),
+        make_tuple("Ficus.nwk.1", "Ficus.data.1", 283815),
+        make_tuple("Ficus.nwk.2", "Ficus.data.2", 851445),
+        make_tuple("Ficus.nwk.3", "Ficus.data.3", 851445), make_tuple("Iris.nwk", "Iris.data", 1),
+        make_tuple("Meredith.mammals.nwk", "Meredith.mammals.data", 1),
+        make_tuple("Meusemann.nwk", "Meusemann.data", 1),
+        make_tuple("Miadlikowska.fungi.nwk", "Miadlikowska.fungi.data", 11655),
+        make_tuple("Misof.insects.nwk", "Misof.insects.data", 1),
+        make_tuple("Primula.nwk", "Primula.data", 2835),
+        make_tuple("Pyron.nwk", "Pyron.data", 2205),
+        make_tuple("Rabosky.scincids.nwk", "Rabosky.scincids.data", 3),
+        make_tuple("Ranunculus.nwk", "Ranunculus.data", 3),
+        make_tuple("Rhododendron.nwk", "Rhododendron.data", 81),
+        make_tuple("Rosaceae.nwk", "Rosaceae.data", mpz_class{"171924246129452853684375"}),
+        make_tuple("Shi.bats.nwk", "Shi.bats.data",
+                   mpz_class{"242234933881884349476362274404296875"}),
+        make_tuple("Solanum.nwk", "Solanum.data", 211865625),
+        make_tuple("Springer.primates.nwk", "Springer.primates.data", 70840575),
+        make_tuple("Szygium.nwk.1", "Szygium.data.1", 45),
+        make_tuple("Szygium.nwk.2", "Szygium.data.2", 45),
+        make_tuple("Tolley.chameleons.nwk", "Tolley.chameleons.data", 1),
+        make_tuple("Wick.1kp.few.genes.nwk", "Wick.1kp.few.genes.data", 1),
+        make_tuple("Wick.1kp.many.genes.nwk", "Wick.1kp.many.genes.data", 1),
+        make_tuple("Yang.caryo.1122.nwk", "Yang.caryo.1122.data", 1),
+        make_tuple("Yang.caryo.209.nwk", "Yang.caryo.209.data", 1)};
 
 TEST_CASE("app", "[app], [.]") {
 	for (auto set : data_sets) {
