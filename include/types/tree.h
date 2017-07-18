7d6a24fd549a841c7449337e712c669dd467ea43
Rudolf Biczok <rcbiczok@gmail.com>
2017-07-18 12:52:27 +0200

    Refactor tree classes

--- include/types/tree.h
+++ include/types/tree.h
@@ -5,49 +5,38 @@
 
 class Tree {
 public:
-	Tree(std::shared_ptr<Tree> p_left, std::shared_ptr<Tree> p_right) :
-			left(p_left), right(p_right) {
-	}
-
-	Tree(leaf_number p_id) :
-			id(p_id), left(nullptr), right(nullptr) {
-	}
-
-	Tree() :
-			left(nullptr), right(nullptr) {
-	}
-
-	leaf_number id;
-	std::shared_ptr<Tree> left;
-	std::shared_ptr<Tree> right;
-	std::weak_ptr<Tree> parent;
-
-	inline bool is_leaf() const {
-		return (left == nullptr && right == nullptr);
-	}
-
-	std::string to_newick_string(const std::vector<std::string> &id_to_lable) const;
-	std::string to_newick_string(const std::vector<std::string> &id_to_lable, const std::string &root_label) const;
-
-    template<typename T>
-    using TreeNodeVisitor = T (*)(const Tree*);
-
-    template<typename T>
-    T fold (BinaryOperator<T> op, TreeNodeVisitor<T> v) const {
-        if(this->is_leaf()) {
-            return v(this);
-        }
-
-        assert(this->left != nullptr);
-        assert(this->right != nullptr);
-        return op(this->left->fold(op, v),
-                  this->right->fold(op, v));
+    Tree(std::shared_ptr<Tree> p_left, std::shared_ptr<Tree> p_right) :
+            left(p_left), right(p_right) {
     }
 
-    size_t count() const;
+    Tree(leaf_number p_id) :
+            id(p_id), left(nullptr), right(nullptr) {
+    }
+
+    Tree() :
+            left(nullptr), right(nullptr) {
+    }
+
+    leaf_number id;
+    std::shared_ptr<Tree> left;
+    std::shared_ptr<Tree> right;
+    std::weak_ptr<Tree> parent;
+
+    inline bool is_leaf() const {
+        return (left == nullptr && right == nullptr);
+    }
+
+    std::string to_newick_string(const std::vector<std::string> &id_to_lable) const;
+
+    std::string to_newick_string(const std::vector<std::string> &id_to_lable,
+                                 const std::string &root_label) const;
+
+protected:
+    void to_newick_string(std::stringstream &ss,
+                          const std::vector<std::string> &id_to_lable) const;
 };
 
-std::ostream& operator<<(std::ostream &strm, const std::shared_ptr<Tree> tree);
+std::ostream &operator<<(std::ostream &strm, const std::shared_ptr<Tree> tree);
 
 std::shared_ptr<Tree> root(const std::shared_ptr<Tree> tree);
 
--- src/types/tree.cpp
+++ src/types/tree.cpp
@@ -4,18 +4,17 @@
 #include <map>
 #include "debug.h"
 
-static void to_newick_string_rec(std::stringstream &ss,
-                                 const std::vector<std::string> &ids_to_lables,
-                                 const Tree &node) {
-	if(node.is_leaf()) {
-		ss << ids_to_lables[node.id];
-	} else {
-		ss << "(";
-		to_newick_string_rec(ss, ids_to_lables, *node.left);
-		ss << ",";
-		to_newick_string_rec(ss, ids_to_lables, *node.right);
-		ss << ")";
-	}
+void Tree::to_newick_string(std::stringstream &ss,
+                                   const std::vector<std::string> &ids_to_lables) const {
+    if (this->is_leaf()) {
+        ss << ids_to_lables[this->id];
+    } else {
+        ss << "(";
+        this->left->to_newick_string(ss, ids_to_lables);
+        ss << ",";
+        this->right->to_newick_string(ss, ids_to_lables);
+        ss << ")";
+    }
 }
 
 std::shared_ptr<Tree> root(std::shared_ptr<Tree> t) {
@@ -27,97 +26,87 @@ std::shared_ptr<Tree> root(std::shared_ptr<Tree> t) {
 }
 
 std::string Tree::to_newick_string(const std::vector<std::string> &ids_to_lables) const {
-	std::stringstream ss;
-	to_newick_string_rec(ss, ids_to_lables, *this);
-	ss << ";";
-	return ss.str();
+    std::stringstream ss;
+    this->to_newick_string(ss, ids_to_lables);
+    ss << ";";
+    return ss.str();
 }
 
 std::string Tree::to_newick_string(const std::vector<std::string> &ids_to_lables,
                                    const std::string &root_label) const {
-	std::stringstream ss;
-	ss << "(";
-	ss << root_label;
-	ss << ",";
-	if(this->is_leaf()) {
-		ss << ids_to_lables[this->id];
-	} else {
-		to_newick_string_rec(ss, ids_to_lables, *this->left);
-		ss << ",";
-		to_newick_string_rec(ss, ids_to_lables, *this->right);
-	}
-	ss << ");";
-	return ss.str();
-}
-
-size_t Tree::count() const {
-    BinaryOperator<size_t> op = [](const size_t a, const size_t b){
-        return a + b;
-    };
-    TreeNodeVisitor<size_t> v = [](auto){
-        return static_cast<size_t>(1);
-    };
-    return this->fold(op, v);
+    std::stringstream ss;
+    ss << "(";
+    ss << root_label;
+    ss << ",";
+    if (this->is_leaf()) {
+        ss << ids_to_lables[this->id];
+    } else {
+        this->left->to_newick_string(ss, ids_to_lables);
+        ss << ",";
+        this->right->to_newick_string(ss, ids_to_lables);
+    }
+    ss << ");";
+    return ss.str();
 }
 
 static std::shared_ptr<Tree> deep_copy(std::shared_ptr<Tree> tree,
-		std::map<std::shared_ptr<Tree>, std::shared_ptr<Tree>> &cover_map) {
-	if (tree == nullptr) {
-		return nullptr;
-	}
-	if (cover_map.count(tree) > 0) {
-		return cover_map[tree];
-	}
+                                       std::map<std::shared_ptr<Tree>, std::shared_ptr<Tree>> &cover_map) {
+    if (tree == nullptr) {
+        return nullptr;
+    }
+    if (cover_map.count(tree) > 0) {
+        return cover_map[tree];
+    }
 
-	auto node = std::make_shared<Tree>();
-	node->id = tree->id;
-	cover_map[tree] = node;
-	cover_map[node] = node;
+    auto node = std::make_shared<Tree>();
+    node->id = tree->id;
+    cover_map[tree] = node;
+    cover_map[node] = node;
 
-	node->left = deep_copy(tree->left, cover_map);
-	node->right = deep_copy(tree->right, cover_map);
-	node->parent = deep_copy(tree->parent.lock(), cover_map);
+    node->left = deep_copy(tree->left, cover_map);
+    node->right = deep_copy(tree->right, cover_map);
+    node->parent = deep_copy(tree->parent.lock(), cover_map);
 
-	return node;
+    return node;
 }
 
 std::tuple<std::shared_ptr<Tree>, std::shared_ptr<Tree>> deep_copy(std::shared_ptr<Tree> tree) {
-	std::map<std::shared_ptr<Tree>, std::shared_ptr<Tree>> cover_map;
+    std::map<std::shared_ptr<Tree>, std::shared_ptr<Tree>> cover_map;
     auto result = deep_copy(tree, cover_map);
-	return std::make_tuple(result, root(result));
+    return std::make_tuple(result, root(result));
 }
 
 static void d_print_tree_rec(std::ostream &strm,
-                                    const std::shared_ptr<Tree> tree,
-                                    const int depth) {
+                             const std::shared_ptr<Tree> tree,
+                             const int depth) {
     assert(
             depth == 1
             || (tree->parent.lock() != nullptr
                 && (tree->parent.lock()->left == tree
                     || tree->parent.lock()->right == tree)));
 
-    if(tree->is_leaf()) {
+    if (tree->is_leaf()) {
         strm << "Label:" << tree->id << std::endl;
     } else {
         strm << "Node" << std::endl;
     }
-	if (tree->left != nullptr) {
-		for (int j = 0; j < depth * 4; j++) {
+    if (tree->left != nullptr) {
+        for (int j = 0; j < depth * 4; j++) {
             strm << " ";
-		}
+        }
         strm << "L:";
-		d_print_tree_rec(strm, tree->left, depth + 1);
-	}
-	if (tree->right != nullptr) {
-		for (int j = 0; j < depth * 4; j++) {
-			strm << " ";
-		}
-		strm << "R:";
-		d_print_tree_rec(strm, tree->right, depth + 1);
-	}
+        d_print_tree_rec(strm, tree->left, depth + 1);
+    }
+    if (tree->right != nullptr) {
+        for (int j = 0; j < depth * 4; j++) {
+            strm << " ";
+        }
+        strm << "R:";
+        d_print_tree_rec(strm, tree->right, depth + 1);
+    }
 }
 
-std::ostream& operator<<(std::ostream &strm, const std::shared_ptr<Tree> tree) {
+std::ostream &operator<<(std::ostream &strm, const std::shared_ptr<Tree> tree) {
     strm << "Dump Tree:" << std::endl;
     d_print_tree_rec(strm, tree, 1);
     return strm << std::endl;
