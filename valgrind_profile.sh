fa2e4381ba819621219b1a8012ea6dbf0f1b05b0
Rudolf Biczok <rcbiczok@gmail.com>
2017-07-18 12:15:21 +0200

    Update valgrind script to run with expensive example

--- valgrind_profile.sh
+++ valgrind_profile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-valgrind --tool=callgrind cmake-build-debug/terraces_main input/modified/Allium_Reduced.data input/modified/Allium_Reduced.nwk
+valgrind --tool=callgrind cmake-build-debug/terraces_main input/modified/Burleigh.birds.small.data input/modified/Burleigh.birds.small.nwk
