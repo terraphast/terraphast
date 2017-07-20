73b8adfb62ecefe5c1a42fd206b4ce00aea547a5
Rudolf Biczok <rcbiczok@gmail.com>
2017-07-20 23:05:15 +0200

    Update csv generator script

--- input/comparison_data/run_tests.sh
+++ input/comparison_data/run_tests.sh
@@ -1,1 +1,6 @@
-for tree in *.nwk*; do data=${tree%.nwk*}.data${tree#*.nwk}; echo "\"${tree}\",\"${data}\",$(../../cmake-build-release/terraces_main $data $tree)"; done > output.csv
+#!/bin/bash
+echo "\"Newick File\",\"Data File\",\"Number of unrooted trees on terrace\"" > output.csv
+for tree in *.nwk*; do
+    data=${tree%.nwk*}.data${tree#*.nwk};
+    echo "\"${tree}\",\"${data}\",$(../../../cmake-build-release/terraces_main $data $tree)";
+done >> output.csv
