e46ecd71b1e3672a4bfb7295a1c050fd3c0a2975
Tobias Ribizel <tobias.ribizel@student.kit.edu>
2017-07-21 13:50:11 +0200

    make unnecessarily slow test faster

--- test/multitree_iterator.cpp
+++ test/multitree_iterator.cpp
@@ -27,14 +27,14 @@ TEST_CASE("multitree_iterator init simple", "[multitree]") {
 }
 
 TEST_CASE("multitree_iterator init unconstrained", "[multitree]") {
-	name_map names{"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+	name_map names{"1", "2", "3", "4", "5", "6", "7", "8"};
 	constraints constraints{{0, 1, 2}};
 	index root_species = 0;
 	tree_enumerator<variants::multitree_callback> enumerator{
 	        {}, names.size(), constraints.size()};
 	auto result = enumerator.run(names.size(), constraints, root_species);
 
-	check_unique_trees(result, count_unrooted_trees<index>(8));
+	check_unique_trees(result, count_unrooted_trees<index>(7));
 }
 
 } // namespace tests
