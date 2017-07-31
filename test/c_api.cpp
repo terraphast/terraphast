0ace0cccd683ad26a8fb11e622e55f442519ba20
Tobias Ribizel <tobias.ribizel@student.kit.edu>
2017-07-31 21:34:49 +0200

    add (optional) tests for c API (actually the simple API)

---
+++ test/c_api.cpp
@@ -0,0 +1,35 @@
+#include <catch.hpp>
+
+#include <terraces/simple.hpp>
+
+#include "datasets.hpp"
+
+namespace terraces {
+namespace tests {
+
+TEST_CASE("c_api_bigint", "[c-api][.]") {
+	for (auto set : data_sets) {
+		auto count = simple::get_terrace_size_bigint_from_file(
+		        "../../data/data/" + std::get<0>(set),
+		        "../../data/data/" + std::get<1>(set));
+		SECTION(std::get<0>(set)) { CHECK(count == std::get<2>(set)); }
+	}
+}
+
+TEST_CASE("c_api", "[c-api][.]") {
+	for (auto set : data_sets) {
+		auto count =
+		        simple::get_terrace_size_from_file("../../data/data/" + std::get<0>(set),
+		                                           "../../data/data/" + std::get<1>(set));
+		SECTION(std::get<0>(set)) {
+			if (count == std::numeric_limits<std::uint64_t>::max()) {
+				CHECK(count <= std::get<2>(set));
+			} else {
+				CHECK(count == std::get<2>(set));
+			}
+		}
+	}
+}
+
+} // namespace tests
+} // namespace terraces
