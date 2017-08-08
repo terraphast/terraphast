8dcbae08546da1ed9f98cec71bc57969f8e41822
Rudolf Biczok <rcbiczok@gmail.com>
2017-08-08 16:27:13 +0200

    Fix gcc warnings

--- src/input_parser.cpp
+++ src/input_parser.cpp
@@ -11,11 +11,11 @@
 
 void free_input_data(input_data *d) {
     for (size_t i = 0; i < d->number_of_species; ++i) {
-        delete [] d->names[i];
+        delete[] d->names[i];
     }
 
-    delete [] d->names;
-    delete [] d->matrix;
+    delete[] d->names;
+    delete[] d->matrix;
     delete d;
 }
 
@@ -40,7 +40,12 @@ char *read_newk_tree(const char *newk_file) {
 
     char *string = new char[static_cast<size_t>(fsize) + 1];
 
-    fread(string, static_cast<size_t>(fsize), 1, f);
+    auto read = fread(string, static_cast<size_t>(fsize), 1, f);
+    if (read <= 0) {
+        delete [] string;
+        fclose(f);
+        return NULL;
+    }
     string[static_cast<size_t>(fsize)] = 0;
 
     fclose(f);
@@ -63,24 +68,40 @@ input_data *parse_input_data(const char *data_file) {
     char input_buffer[256];
 
     if (result != NULL) {
-        fscanf(f, "%zu %zu", &(result->number_of_species), &(result->number_of_partitions));
+        auto read = fscanf(f, "%zu %zu", &(result->number_of_species), &(result->number_of_partitions));
+        if (read != 2) {
+            delete result;
+            fclose(f);
+            return NULL;
+        }
 
         result->matrix = new unsigned char[result->number_of_species * result->number_of_partitions];
         result->names = new char *[result->number_of_species];
 
         for (size_t s = 0; s < result->number_of_species; ++s) {
             for (size_t p = 0; p < result->number_of_partitions; ++p) {
-                fscanf(f, "%1hhu", &result->matrix[s * result->number_of_partitions + p]);
+                read = fscanf(f, "%1hhu", &result->matrix[s * result->number_of_partitions + p]);
+                if (read != 1) {
+                    delete result;
+                    fclose(f);
+                    return NULL;
+                }
             }
 
             // discard one (whitespace)
             fgetc(f);
 
-            fgets(input_buffer, 256, f);
+            auto readStr = fgets(input_buffer, 256, f);
+            if(readStr == NULL) {
+                delete result;
+                fclose(f);
+                return NULL;
+            }
 
-            if(feof(f)!=0) {
-              // end of line reached, but there is more to read (num_species is too big)
-              return NULL;
+
+            if (feof(f) != 0) {
+                // end of line reached, but there is more to read (num_species is too big)
+                return NULL;
             }
 
             size_t len = strlen(input_buffer);
@@ -94,8 +115,8 @@ input_data *parse_input_data(const char *data_file) {
             }
         }
     }
-    fgets(input_buffer, 256, f);
-    if(feof(f) == 0) {
+    auto readStr = fgets(input_buffer, 256, f);
+    if (feof(f) == 0 && readStr != NULL) {
         // end of line is not reached yet, but there are no more species to read (num_species is too small)
         return NULL;
     }
@@ -118,8 +139,8 @@ ntree_t *get_newk_tree(const char *nwk_file) {
 
 ntree_t *get_newk_tree_from_string(const char *nwk_string) {
     ntree_t *tree = ntree_parse_newick_from_string(nwk_string);
-    if(tree == nullptr) {
-      return nullptr;
+    if (tree == nullptr) {
+        return nullptr;
     }
     fix_tree(tree); //necessary since tree seems to be corrupt
     return tree;
